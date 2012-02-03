f2187756e9587e18d955438b95bc8a5709af4cea
Alexandre Gaigalas <alexandre@gaigalas.net>
2012-02-03 19:31:27 -0200

    Less workarounds in

--- library/Respect/Http/Client.php
+++ library/Respect/Http/Client.php
@@ -4,16 +4,19 @@ namespace Respect\Http;
 
 class Client
 {
+	static $globalHeaders = false;
 	static function __callStatic($method, $arguments)
 	{
-		global $http_response_header;
-		$http_response_header = array();
 		list($uri) = $arguments;
 		$context = stream_context_create(array(
 			'http' => array(
 				'method' => strtoupper($method)
 			)
 		));
-		return new Response(file_get_contents($uri, false, $context), $http_response_header);
+		return new Response(
+			file_get_contents($uri, false, $context), 
+			//we need this in order to be testable, sorry
+			static::$globalHeaders ? $GLOBALS['http_response_header'] : $http_response_header
+		);
 	}
 }
--- tests/library/Respect/Http/ClientTest.php
+++ tests/library/Respect/Http/ClientTest.php
@@ -11,15 +11,22 @@ function file_get_contents()
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
+	protected function tearDown() 
+	{
+		Client::$globalHeaders = false;
+	}
 	protected function mockFileGetContents($contents, array $headers=array())
 	{
 		global $TEST_RESPECT_HTTP_BODY, $TEST_RESPECT_HTTP_HEADERS;
+		Client::$globalHeaders = true;
 		list($TEST_RESPECT_HTTP_BODY, $TEST_RESPECT_HTTP_HEADERS) = func_get_args();
 		return $contents;
 	}
 	function test_client_can_be_used_statically()
 	{
 		$this->mockFileGetContents('<html>FooBody', array('Host'=>'example.com'));
+
+		//the test itself
 		$r = Client::get('/foo');
 		$this->assertEquals('<html>FooBody', (string) $r);
 		$this->assertEquals($r['Host'], 'example.com');
